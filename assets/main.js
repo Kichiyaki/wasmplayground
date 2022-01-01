699b0dedcc885b42598fa4f65a435b55b56846d8
Kichiyaki <xyztojajestem@gmail.com>
2022-01-01 08:49:34 +0100

    set event listeners from WASM

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,7 @@
-const addEventListeners = () => {
-  document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const a = parseFloat(e.target.a.value);
-    const b = parseFloat(e.target.b.value);
-
-    document.querySelector("#result").innerHTML = add(a, b);
-  });
-};
-
-const run = () => {
-  addEventListeners();
-};
-
 const go = new Go();
 
 WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
   (res) => {
     go.run(res.instance);
-
-    run();
   }
 );
--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,31 +1,80 @@
 package main
 
 import (
-	"log"
+	"strconv"
 	"syscall/js"
 )
 
 func main() {
 	ch := make(chan struct{}, 0)
 
-	log.Println("Hello from Go!")
-	js.Global().Set("add", add())
+	doc := js.Global().Get("document")
+
+	a := newApp(doc)
+	a.start()
 
 	<-ch
 }
 
-func add() js.Func {
+type app struct {
+	doc js.Value
+}
+
+func newApp(doc js.Value) *app {
+	return &app{
+		doc,
+	}
+}
+
+func (a *app) start() {
+	a.addEventListeners()
+}
+
+func (a *app) addEventListeners() {
+	form := a.doc.Call("querySelector", "form")
+
+	form.Call("addEventListener", "submit", a.add())
+}
+
+func (a *app) add() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		var sum float64
-
-		for _, arg := range args {
-			if arg.Type() != js.TypeNumber || arg.IsNaN() {
-				return 0
-			}
-
-			sum += arg.Float()
+		if len(args) != 1 || args[0].Type() != js.TypeObject {
+			return nil
 		}
 
-		return sum
+		ev := args[0]
+		ev.Call("preventDefault")
+
+		aStr := ev.Get("target").Get("a").Get("value").String()
+		bStr := ev.Get("target").Get("b").Get("value").String()
+
+		aVal, err := strconv.ParseFloat(aStr, 10)
+		if err != nil {
+			a.showError(err.Error())
+			return nil
+		}
+
+		bVal, err := strconv.ParseFloat(bStr, 10)
+		if err != nil {
+			a.showError(err.Error())
+			return nil
+		}
+
+		a.showError("")
+		a.showResult(aVal + bVal)
+
+		return nil
 	})
+}
+
+func (a *app) showResult(result float64) {
+	p := a.doc.Call("querySelector", "#result")
+
+	p.Set("innerHTML", result)
+}
+
+func (a *app) showError(msg string) {
+	p := a.doc.Call("querySelector", "#error")
+
+	p.Set("innerHTML", msg)
 }
